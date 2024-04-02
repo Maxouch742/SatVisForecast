a6b1c12b1e5d395e0eabab3926c38fd1857b552b
Maxime Fourquaux <maxime.fourquaux@heig-vd.ch>
2024-04-02 12:39:42 +0200

    Set public path

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+/*module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/SatVisForecast/" : "/",
 })
+*/
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/SatVisForecast' : '/',
+}
